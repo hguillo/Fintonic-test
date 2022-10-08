70b00384ccc1fb62344063fcd7fd3885278cc02f
Hector Guillo Anton <hectorguillo@gmail.com>
2022-10-08 11:08:04 +0200

    DB connection and server start

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
+require('dotenv').config();
+
 const express = require('express');
+const mongoClient = require('mongoose');
 
 const app = express();
 
+app.set('port', process.env.PORT || 3000);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
+
+
+mongoClient.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => {
+    console.log('DB connected...');
+    app.listen(app.get('port'), () => {
+      console.log(`App is running at http://localhost:${app.get('port')} in ${app.get('env')} mode`);
+      console.log('  Press CTRL-C to stop\n');
+  });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
